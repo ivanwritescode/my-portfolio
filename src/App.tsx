45c72bbacfcb98efe3e9282992b73380dc7d77b8
ivanwritescode <ivanmejico.dev@gmail.com>
2023-05-25 22:04:36 +0800

    Header style changes. Added react-bootstrap-icons support.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import { Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/App.scss'
+import { Container } from 'react-bootstrap'
+import { Github, Linkedin, Twitter } from "react-bootstrap-icons";
+import { TryHackMeIcon, CodewarsIcon } from './Components/icons/Icons';
 
 function App() {
 
@@ -7,9 +10,9 @@ function App() {
     <>
       <header id="header">
         <Container>
-          <h1><a href='/'>Ivan Mejico</a></h1>
+          <h1><a href='/my-portfolio/'>Ivan Mejico</a></h1>
           <h2>I'm a passionate <span>web developer</span></h2>
-          <nav>
+          <nav id="navbar" className='navbar'>
             <ul>
               <li><a href="#home">Home</a></li>
               <li><a href="#about">About</a></li>
@@ -19,12 +22,12 @@ function App() {
               <li><a href="#contact">Contact</a></li>
             </ul>
           </nav>
-          <div>
-            <a href="twitter.com/ivan">twtricon</a>
-            <a href="facebook.com/ivan">fbicon</a>
-            <a href="linkedin.com/ivan">linkeinicon</a>
-            <a href="github.com/ivan">githubicon</a>
-            <a href="ivansottomejico.com">websiteicon</a>
+          <div className="social-links">
+            <a href="https://twitter.com/ivanwritescode" target="_blank"><Twitter/></a>
+            <a href="https://github.com/ivanwritescode" target="_blank"><Github/></a>
+            <a href="https://linkedin.com/in/ivan-mejico" target="_blank"><Linkedin/></a>
+            <a href="https://tryhackme.com/p/Nemos" target="_blank"><TryHackMeIcon/></a>
+            <a href="https://www.codewars.com/users/ivanwritescode" target="_blank"><CodewarsIcon/></a>
           </div>
         </Container>
       </header>
