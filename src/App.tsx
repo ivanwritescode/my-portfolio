d837aad93de5696cc28cd156bf3805cf47cfc00c
ivanwritescode <ivanmejico.dev@gmail.com>
2023-05-26 09:17:14 +0800

    Modified nav using React Bootstrap Nav code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/App.scss'
-import { Container } from 'react-bootstrap'
+import { Container, Nav } from 'react-bootstrap'
 import { Github, Linkedin, Twitter } from "react-bootstrap-icons";
 import { TryHackMeIcon, CodewarsIcon } from './Components/icons/Icons';
 
@@ -12,22 +12,22 @@ function App() {
         <Container>
           <h1><a href='/my-portfolio/'>Ivan Mejico</a></h1>
           <h2>I'm a passionate <span>web developer</span></h2>
-          <nav id="navbar" className='navbar'>
-            <ul>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#resume">Resume</a></li>
-              <li><a href="#services">Services</a></li>
-              <li><a href="#portfolio">Portfolio</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </nav>
+          
+          <Nav id="navbar" className="navbar">
+            <Nav.Item><Nav.Link>Home</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link>About</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link>Resume</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link>Services</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link>Portfolio</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link>Contact</Nav.Link></Nav.Item>
+          </Nav>
+
           <div className="social-links">
-            <a href="https://twitter.com/ivanwritescode" target="_blank"><Twitter/></a>
-            <a href="https://github.com/ivanwritescode" target="_blank"><Github/></a>
-            <a href="https://linkedin.com/in/ivan-mejico" target="_blank"><Linkedin/></a>
-            <a href="https://tryhackme.com/p/Nemos" target="_blank"><TryHackMeIcon/></a>
-            <a href="https://www.codewars.com/users/ivanwritescode" target="_blank"><CodewarsIcon/></a>
+            <a href="https://twitter.com/ivanwritescode" target="_blank"><Twitter /></a>
+            <a href="https://github.com/ivanwritescode" target="_blank"><Github /></a>
+            <a href="https://linkedin.com/in/ivan-mejico" target="_blank"><Linkedin /></a>
+            <a href="https://tryhackme.com/p/Nemos" target="_blank"><TryHackMeIcon /></a>
+            <a href="https://www.codewars.com/users/ivanwritescode" target="_blank"><CodewarsIcon /></a>
           </div>
         </Container>
       </header>
