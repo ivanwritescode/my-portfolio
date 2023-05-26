09ac6ae5d32399e51e2010f66446452ce85af7df
ivanwritescode <ivanmejico.dev@gmail.com>
2023-05-26 09:25:33 +0800

    Replaced <a> tags with <Nav.Link>

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
         <Container>
           <h1><a href='/my-portfolio/'>Ivan Mejico</a></h1>
           <h2>I'm a passionate <span>web developer</span></h2>
-          
           <Nav id="navbar" className="navbar">
             <Nav.Item><Nav.Link>Home</Nav.Link></Nav.Item>
             <Nav.Item><Nav.Link>About</Nav.Link></Nav.Item>
@@ -21,13 +20,12 @@ function App() {
             <Nav.Item><Nav.Link>Portfolio</Nav.Link></Nav.Item>
             <Nav.Item><Nav.Link>Contact</Nav.Link></Nav.Item>
           </Nav>
-
           <div className="social-links">
-            <a href="https://twitter.com/ivanwritescode" target="_blank"><Twitter /></a>
-            <a href="https://github.com/ivanwritescode" target="_blank"><Github /></a>
-            <a href="https://linkedin.com/in/ivan-mejico" target="_blank"><Linkedin /></a>
-            <a href="https://tryhackme.com/p/Nemos" target="_blank"><TryHackMeIcon /></a>
-            <a href="https://www.codewars.com/users/ivanwritescode" target="_blank"><CodewarsIcon /></a>
+            <Nav.Link href="https://twitter.com/ivanwritescode" target="_blank"><Twitter /></Nav.Link>
+            <Nav.Link href="https://github.com/ivanwritescode" target="_blank"><Github /></Nav.Link>
+            <Nav.Link href="https://linkedin.com/in/ivan-mejico" target="_blank"><Linkedin /></Nav.Link>
+            <Nav.Link href="https://tryhackme.com/p/Nemos" target="_blank"><TryHackMeIcon /></Nav.Link>
+            <Nav.Link href="https://www.codewars.com/users/ivanwritescode" target="_blank"><CodewarsIcon /></Nav.Link>
           </div>
         </Container>
       </header>
