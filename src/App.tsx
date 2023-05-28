9199d2cb71663304476359962419d6944b799dbe
ivanwritescode <ivanmejico.dev@gmail.com>
2023-05-28 10:43:40 +0800

    Fixed the social link icons with bootstrap classes

--- src/App.tsx
+++ src/App.tsx
@@ -31,9 +31,9 @@ function App() {
           </nav>
 
           <div className="social-links">
-            <a href="https://twitter.com/ivanwritescode" target="_blank"></a>
-            <a href="https://github.com/ivanwritescode" target="_blank"></a>
-            <a href="https://linkedin.com/in/ivan-mejico" target="_blank"></a>
+            <a href="https://twitter.com/ivanwritescode" target="_blank"><i className='bi bi-twitter'></i></a>
+            <a href="https://github.com/ivanwritescode" target="_blank"><i className='bi bi-facebook'></i></a>
+            <a href="https://linkedin.com/in/ivan-mejico" target="_blank"><i className='bi bi-linkedin'></i></a>
             <a href="https://tryhackme.com/p/Nemos" target="_blank"><TryHackMeIcon /></a>
             <a href="https://www.codewars.com/users/ivanwritescode" target="_blank"><CodewarsIcon /></a>
           </div>
