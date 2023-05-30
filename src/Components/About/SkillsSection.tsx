2e23ba10d907c2353a4bf804693c978e52f9ffd4
ivanwritescode <ivanmejico.dev@gmail.com>
2023-05-30 11:43:21 +0800

    Added render logic to Skills section

--- src/Components/About/SkillsSection.tsx
+++ src/Components/About/SkillsSection.tsx
@@ -1,93 +1,58 @@
+import { ReactNode } from "react"
+
+interface IColumnWrapperProps {
+    children: ReactNode;
+}
+
+const ColumnWrapper = (props: IColumnWrapperProps) => {
+    return (
+        <div className="col-lg-6">
+            { props.children }
+        </div>
+    )
+}
+
+
 const SkillsSection = () => {
+    const skills = [
+        { name: "GitHub and Git Version Control", proficiency: 60 },
+        { name: "HTML, CSS, and JavaScript", proficiency: 60 },
+        { name: "CSS Preprocessors (SASS/SCSS)", proficiency: 40 },
+        { name: "Bootstrap", proficiency: 40 },
+        { name: "Material-UI", proficiency: 20 },
+        { name: "React and Redux", proficiency: 40 },
+        { name: "AJAX, socket.io, async/await, and axios", proficiency: 40 },
+        { name: "Web Security", proficiency: 40 },
+        { name: "PHP, MySQL, Apache backend", proficiency: 40 },
+        { name: "Github Actions CI/CD", proficiency: 40 },
+        { name: "npm, webpack, and babel", proficiency: 20 },
+        { name: "Photoshop and Adobe Illustrator", proficiency: 20 },
+        { name: "GitLab", proficiency: 10 },
+        { name: "TeamCity CI/CD", proficiency: 10 },
+    ] 
+
     return (
         <div className="skills container">
             <div className="section-title">
                 <h2>skills</h2>
             </div>
             <div className="row skills-content">
-                <div className="col-lg-6">
-                    <div className="progress">
-                        <span className="skill">HTML <i className="val">90%</i></span>
-                        <div className="progress-bar-wrap">
-                            <div
-                                className="progress-bar"
-                                style={{ width: "90%" }}
-                                role="progressbar"
-                                aria-valuenow={ 100 }
-                                aria-valuemin={ 0 }
-                                aria-valuemax={ 100 }>
+                { skills.map((skill) => 
+                    <ColumnWrapper>
+                        <div className="progress">
+                            <span className="skill">{skill.name} <i className="val">{skill.proficiency}%</i></span>
+                            <div className="progress-bar-wrap">
+                                <div
+                                    className="progress-bar"
+                                    style={{ width: `${skill.proficiency}%` }}
+                                    role="progressbar"
+                                    aria-valuenow={ 100 }
+                                    aria-valuemin={ 0 }
+                                    aria-valuemax={ 100 }>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="progress">
-                        <span className="skill">CSS <i className="val">90%</i></span>
-                        <div className="progress-bar-wrap">
-                            <div
-                                className="progress-bar"
-                                style={{width: "90%"}}
-                                role="progressbar"
-                                aria-valuenow={ 100 }
-                                aria-valuemin={ 0 }
-                                aria-valuemax={ 100 }>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="progress">
-                        <span className="skill">JavaScript <i className="val">60%</i></span>
-                        <div className="progress-bar-wrap">
-                            <div
-                                className="progress-bar"
-                                style={{width: "60%"}}
-                                role="progressbar"
-                                aria-valuenow={ 100 }
-                                aria-valuemin={ 0 }
-                                aria-valuemax={ 100 }>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-lg-6">
-                    <div className="progress">
-                        <span className="skill">React <i className="val">50%</i></span>
-                        <div className="progress-bar-wrap">
-                            <div
-                                className="progress-bar"
-                                style={{width: "50%"}}
-                                role="progressbar"
-                                aria-valuenow={ 100 }
-                                aria-valuemin={ 0 }
-                                aria-valuemax={ 100 }>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="progress">
-                        <span className="skill">Redux <i className="val">40%</i></span>
-                        <div className="progress-bar-wrap">
-                            <div
-                                className="progress-bar"
-                                style={{width: "40%"}}
-                                role="progressbar"
-                                aria-valuenow={ 100 }
-                                aria-valuemin={ 0 }
-                                aria-valuemax={ 100 }>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="progress">
-                        <span className="skill">PHP <i className="val">70%</i></span>
-                        <div className="progress-bar-wrap">
-                            <div
-                                className="progress-bar"
-                                style={{width: "40%"}}
-                                role="progressbar"
-                                aria-valuenow={ 100 }
-                                aria-valuemin={ 0 }
-                                aria-valuemax={ 100 }>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    </ColumnWrapper>) }
             </div>
         </div>
     )
