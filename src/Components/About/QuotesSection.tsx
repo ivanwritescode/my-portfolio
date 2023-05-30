b9d91242a4b8187b539893a27a1962480f16ae16
ivanwritescode <ivanmejico.dev@gmail.com>
2023-05-30 18:43:48 +0800

    Added responsiveness to the swiper

--- src/Components/About/QuotesSection.tsx
+++ src/Components/About/QuotesSection.tsx
@@ -22,6 +22,20 @@ const QuotesSection = () => {
                     delay: 3000,
                     disableOnInteraction: false
                 }}
+                breakpoints={{
+                    0: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 40,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 50,
+                    },
+                }}
                 navigation={true}
                 modules={[ Autoplay, Pagination, Navigation ]}
             >
